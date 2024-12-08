4529caf78d702e880dca2bfdffc33d7428126cc3
Gustavo Avila <info.gustavoavila@gmail.com>
2024-12-08 17:57:24 -0500

    home

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,14 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+const proyectos = defineCollection({
+	// Load Markdown and MDX files in the `src/content/proyectos/` directory.
+	loader: glob({ base: './src/content/proyectos', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
+		type: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
@@ -15,4 +16,4 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+export const collections = { proyectos };
