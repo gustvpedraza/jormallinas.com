53f807c4e18bed332ec19bd2299c3010a68d5393
info.gustavoavila@gmail.com <info.gustavoavila@gmail.com>
2025-01-16 18:44:00 -0500

    hero fix

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jorma Llinas';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Jorma Llinas | Diseñadora UX/UI | Diseñadora Web';
+export const SITE_DESCRIPTION = 'Ayudo a startups, agencias de diseño y pequeñas empresas a convertir ideas en aplicaciones y páginas web responsive, optimizadas para ofrecer experiencias intuitivas que atraen y retienen usuarios.';
